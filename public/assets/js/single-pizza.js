d51efcf8cdd16df60f56ec03fbd1c1776a9ed187
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-06-25 13:44:46 -0400

    Update comment submit function

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -109,30 +109,29 @@ function handleNewCommentSubmit(event) {
   }
 
   const formData = { commentBody, writtenBy };
+
+  fetch(`/api/comments/${pizzaId}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
-
-function handleNewReplySubmit(event) {
-  event.preventDefault();
-
-  if (!event.target.matches('.reply-form')) {
-    return false;
-  }
-
-  const commentId = event.target.getAttribute('data-commentid');
-
-  const writtenBy = event.target.querySelector('[name=reply-name]').value;
-  const replyBody = event.target.querySelector('[name=reply]').value;
-
-  if (!replyBody || !writtenBy) {
-    return false;
-  }
-
-  const formData = { writtenBy, replyBody };
-}
-
-$backBtn.addEventListener('click', function() {
-  window.history.back();
-});
 
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
